cdcd533e392e577aa3c421d78d64712da5663e43
Нурсултан Алмаханов <nurik-1998kz@outlook.com>
2017-01-21 02:52:52 +0600

    commit init

---
+++ YTREWQ/YTREWQ/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YTREWQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("wertyu");
+        }
+    }
+}
